da1e6278321519792f158eba01a9e768730cb740
Cybertron123-tech <71644024+Cybertron123-tech@users.noreply.github.com>
2020-12-10 16:21:17 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,28 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(200, 200, 70, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350, 200, 70, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(500, 200, 70, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(275, 290, 70, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(425, 290, 70, 0, 2 * Math.PI);
+ctx.stroke();
